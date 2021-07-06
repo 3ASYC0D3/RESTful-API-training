294568f0a2c053593084a54383fabbc1ce0ddaa2
Gulka_D <d.gulka4@gmail.com>
2021-07-06 12:30:08 +0200

    Express instal. Views, public dirs setup. Server check

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.listen(port);
+
+app.get('/check', (req, res) => {
+    res.send('Check')
+});
+
